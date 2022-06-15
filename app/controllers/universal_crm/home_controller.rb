53befe81981dbbaf54b82d700d4fbb629507c34f
Dion Sanderson <dion.sanderson@gmail.com>
2022-06-15 15:35:02 +1000

    remove require_user dependancy for home controller

--- app/controllers/universal_crm/home_controller.rb
+++ app/controllers/universal_crm/home_controller.rb
@@ -3,8 +3,13 @@ require_dependency "universal_crm/application_controller"
 module UniversalCrm
   class HomeController < ApplicationController
     include UniversalCrm::Concerns::Home
-    
-    skip_before_action :require_user, only: %w(inbound)
-    
+
+    before_action :check_crm_access, except: %w(inbound)
+
+    private
+
+    def check_crm_access
+      invalid and return false if !signed_in? or !current_user.has?(:crm)
+    end
   end
 end
