c7fdeb9431924cdcddfb46f4f198e578e6baf7a0
Ben Petro <benpetro@gmail.com>
2024-02-15 11:04:03 +1000

    rubocop fix

--- app/controllers/universal/comments_controller.rb
+++ app/controllers/universal/comments_controller.rb
@@ -1,4 +1,4 @@
-require_dependency "universal/application_controller"
+require_dependency 'universal/application_controller'
 
 module Universal
   class CommentsController < ::UniversalCrm::ApplicationController
@@ -7,16 +7,17 @@ module Universal
     def index
       @model = find_model
       comments = load_comments
-      render json: comments.map{|c| c.to_json}
+      render json: comments.map(&:to_json)
     end
-    
+
     def create
       @model = find_model
-      @comment = @model.comments.new content: params[:content], kind: params[:kind], scope: universal_scope, subject_name: @model.name, subject_kind: @model.kind
+      @comment = @model.comments.new content: params[:content], kind: params[:kind], scope: universal_scope,
+                                     subject_name: @model.name, subject_kind: @model.kind
       @comment.when = Time.now.utc
       @comment.user = current_user
       if @comment.save
-        if @model.class == UniversalCrm::Ticket 
+        if @model.class == UniversalCrm::Ticket
           if @comment.email?
             UniversalCrm::Mailer.ticket_reply(universal_crm_config, @model.subject, @model, @comment).deliver_now
           end
@@ -27,15 +28,15 @@ module Universal
         logger.debug @comment.errors.to_json
       end
       comments = load_comments
-      render json: comments.map{|c| c.to_json}
+      render json: comments.map { |c| c.to_json }
     end
 
     def recent
       @comments = Universal::Comment.unscoped.order_by(created_at: :desc)
-      @comments = @comments.scoped_to(universal_scope) if !universal_scope.nil?
-      @comments = @comments.where(subject_type: params[:subject_type]) if !params[:subject_type].blank?
-      @comments = @comments.where(user_id: params[:user_id]) if !params[:user_id].blank?
-      @comments = @comments.where(subject_kind: params[:subject_kind]) if !params[:subject_kind].blank?
+      @comments = @comments.scoped_to(universal_scope) unless universal_scope.nil?
+      @comments = @comments.where(subject_type: params[:subject_type]) unless params[:subject_type].blank?
+      @comments = @comments.where(user_id: params[:user_id]) unless params[:user_id].blank?
+      @comments = @comments.where(subject_kind: params[:subject_kind]) unless params[:subject_kind].blank?
       @comments = @comments.page(params[:page])
       render json: {
         pagination: {
@@ -44,25 +45,22 @@ module Universal
           current_page: params[:page].to_i,
           per_page: 20
         },
-        comments: @comments.map{|c| c.to_json}
+        comments: @comments.map { |c| c.to_json }
       }
     end
 
     private
+
     def find_model
-      if params[:subject_type]
-        return params[:subject_type].classify.constantize.unscoped.find params[:subject_id]
-      end
-      return nil
+      return params[:subject_type].classify.constantize.unscoped.find params[:subject_id] if params[:subject_type]
+
+      nil
     end
-    
+
     def load_comments
       comments = @model.comments
-      if params[:hide_private_comments].to_s == 'true'
-        comments = comments.not_system_generated.email 
-      end
-      return comments
+      comments = comments.not_system_generated.email if params[:hide_private_comments].to_s == 'true'
+      comments
     end
-    
   end
 end
